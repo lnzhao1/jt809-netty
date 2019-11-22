9df8d55c051821e5a083b18e5417d962a2784d70
zhaoxiao <605753670@qq.com>
2019-11-22 11:12:42 +0800

    demo

---
+++ src/main/java/com/unicom/netty809/Netty809Application.java
@@ -0,0 +1,13 @@
+package com.unicom.netty809;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Netty809Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Netty809Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/unicom/netty809/Netty809ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicom.netty809;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Netty809ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
